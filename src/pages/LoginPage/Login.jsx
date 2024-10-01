1a7f8c0301c25c7ccad8bdab0e97aeb8afb33513
duongne1 <caotrungduong11@gmail.com>
2024-10-01 10:49:21 +0700

    Call-api: Food-Filter

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 const Login = () => {
-    return <div>Login</div>;
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <div className="login-container bg-red-300 w-[500px] h-[422px]">
+            <Form
+                name="login_form"
+                className="login-form bg-[#95989D]"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+            >
+                <h2>ĐĂNG NHẬP</h2>
+                <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: 'Vui lòng nhập số điện thoại hoặc email!' }]}
+                >
+                    <Input prefix={<UserOutlined />} placeholder="Số điện thoại hoặc email" />
+                </Form.Item>
+
+                <Form.Item name="password" rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}>
+                    <Input.Password prefix={<LockOutlined />} placeholder="Mật khẩu" />
+                </Form.Item>
+
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Lưu mật khẩu</Checkbox>
+                    </Form.Item>
+
+                    <button className="login-form-forgot">Quên mật khẩu?</button>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Đăng nhập
+                    </Button>
+                    <div className="register-link">
+                        Không có tài khoản? <button>Đăng ký</button>
+                    </div>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 };
 
 export default Login;
